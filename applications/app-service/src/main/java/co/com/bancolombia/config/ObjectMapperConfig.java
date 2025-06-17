97a242a5ca656964d467fb2beaea10932d15ae5b
Jhon Anthony Ortiz Burbano <78940936+JhonOrtiz0406@users.noreply.github.com>
2025-06-16 20:25:32 -0500

    Rename ObjectMapper config and fix bean

--- applications/app-service/src/main/java/co/com/bancolombia/config/ObjectMapperConfig.java
+++ applications/app-service/src/main/java/co/com/bancolombia/config/ObjectMapperConfig.java
@@ -1,10 +1,13 @@
 package co.com.bancolombia.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ObjectMapper {
+public class ObjectMapperConfig {
 
+    @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
     }
